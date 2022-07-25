bb7c2a8cb5587bce0d1a8c2b36944306c1514720
sorlros <sorlros2047@gmail.com>
2022-07-25 14:51:51 +0900

    redux-advanced-1

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,5 @@
 // 액션의 타입들을 오타를 방지하기 위해 변수로 미리 지정
+import { axios } from "axios"
 export const ADD_TODO = "ADD_TODO"
 export const COMPLETE_TODO = "COMPLETE_TODO"
 
@@ -29,5 +30,58 @@ export function showAll() {
 export function showComplete() {
 	return {
 		type: SHOW_COMPLETE,
+	}
+}
+
+// users
+
+export const GET_USERS_START = "GET_USERS_START" // 깃헙 api 호출을 시작하는 것을 의미
+export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS" // api 호출에 대한 응답이 성공한 경우
+export const GET_USERS_FAIL = "GET_USERS_FAIL" // api 호출에 실패한 경우
+
+export function getUsersStart() {
+	return {
+		type: GET_USERS_START,
+	}
+}
+
+export function getUsersSuccess(data) {
+	return {
+		type: GET_USERS_SUCCESS,
+		data,
+	}
+}
+
+export function getUsersFail(error) {
+	return {
+		type: GET_USERS_FAIL,
+		error,
+	}
+}
+
+export function getUsersThunk() {
+	return async (dispatch) => {
+		try {
+			dispatch(getUsersStart())
+			const res = await axios.get("https://api.github.com/users")
+			dispatch(getUsersSuccess(res.data))
+		} catch (error) {
+			dispatch(getUsersFail(error))
+		}
+	}
+}
+
+export const GET_USERS = "GET_USERS"
+export const GET_USERS_PENDING = "GET_USERS_PENDING"
+export const GET_USERS_FULFILLED = "GET_USERS_FULFILLED"
+export const GET_USERS_REJECTED = "GET_USERS_REJECTED"
+
+export function getUsersPromise() {
+	return {
+		type: GET_USERS,
+		payload: async () => {
+			const res = await axios.get("https://api.github.com/users")
+			return res.data
+		},
 	}
 }
